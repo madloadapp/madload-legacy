627bd6034aa31cb7881b41a6b410a276ef91cc35
Osama <osamaeletch@gmail.com>
2020-02-09 08:48:30 +0200

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  endOfLine: 'lf',
+  printWidth: 80
+};
